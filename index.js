12e65fc4949d612ae9f1b4bbe8abab10fc6fff86
Dmitry Kalinin <vega-kim@mail.ru>
2020-08-30 21:07:06 +0300

    after defined prettier setting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Contaner from "./src/component/Contaner/Container";
+import React from 'react';
+import ReactDom from 'react-dom';
+import Contaner from './src/component/Contaner/Container';
 
-ReactDom.render(<Contaner />, document.getElementById("root"));
+ReactDom.render(<Contaner />, document.getElementById('root'));
--- src/data/CashierCheck.js
+++ src/data/CashierCheck.js
@@ -1,46 +1,46 @@
 class CashierCheck {
-	constructor(firmName = "ООО Нептун") {
-		this.firmName = firmName;
-		this.time = new Date().toLocaleString();
-		this.products = [];
-		this.totalPrice = 0;
-	}
+  constructor(firmName = 'ООО Нептун') {
+    this.firmName = firmName;
+    this.time = new Date().toLocaleString();
+    this.products = [];
+    this.totalPrice = 0;
+  }
 
-	setProducts(productName, productPrise) {
-		if (typeof productName === "string" && typeof productPrise === "number") {
-			let product = { productName, productPrise };
-			this.products.push(product);
-		} else {
-			console.error(new Error("Первое значение строка, второе число"));
-		}
-	}
+  setProducts(productName, productPrise) {
+    if (typeof productName === 'string' && typeof productPrise === 'number') {
+      let product = { productName, productPrise };
+      this.products.push(product);
+    } else {
+      console.error(new Error('Первое значение строка, второе число'));
+    }
+  }
 
-	deleteLastEnter() {
-		this.products.pop();
-	}
+  deleteLastEnter() {
+    this.products.pop();
+  }
 
-	deleteTargetItem(index) {
-		if (typeof index === "number" && this.products[index] !== "undefined") {
-			this.products.splice(index, 1);
-		} else {
-			console.error(new Error("Индекс должен быть числом или не существует"));
-		}
-	}
+  deleteTargetItem(index) {
+    if (typeof index === 'number' && this.products[index] !== 'undefined') {
+      this.products.splice(index, 1);
+    } else {
+      console.error(new Error('Индекс должен быть числом или не существует'));
+    }
+  }
 
-	calcTotalPrice() {
-		for (let i = 0; i < this.products.length; i++) {
-			this.totalPrice += this.products[i].productPrise;
-		}
-	}
+  calcTotalPrice() {
+    for (let i = 0; i < this.products.length; i++) {
+      this.totalPrice += this.products[i].productPrise;
+    }
+  }
 }
 
 let check = new CashierCheck();
-check.setProducts("Картофель", 7);
-check.setProducts("Морковь", 3);
-check.setProducts("Каша", 5);
-check.setProducts("Персики", 10);
-check.setProducts("Бананы", 10);
-check.setProducts("Кифир", 10);
+check.setProducts('Картофель', 7);
+check.setProducts('Морковь', 3);
+check.setProducts('Каша', 5);
+check.setProducts('Персики', 10);
+check.setProducts('Бананы', 10);
+check.setProducts('Кифир', 10);
 check.deleteLastEnter();
 check.deleteTargetItem(0);
 check.calcTotalPrice();
